e375f3a56e98fd4fdadbeb702ab63b61514e2ede
Mark Hoopes <mark@merisec.com>
2022-05-08 03:58:36 +0000

    Updated query metadata.  Skipped several methods.

--- app/models/queries.rb
+++ app/models/queries.rb
@@ -27,14 +27,6 @@ Queries = [
     :input => {:name => :is_admin, :example => "false) OR 1=1 --"},
     :example => "This example bypasses any conditions and deletes all users.",
     :sql => 'SELECT "users".* FROM "users" WHERE (id = NULL AND is_admin = [REPLACE])',
-    :desc => <<-MD
-Any methods which delete records should be used with lots of caution! `destroy_by` is only slightly safer
-than `delete_by` since it will invoke callbacks associated with the model.
-The `destroy_by` method takes the same kind of conditions arguments as `where`.
-The argumene can be a string, an array, or a hash of conditions. Strings will not
-be escaped at all. Use an array or hash to safely parameterize arguments.
-Never pass user input directly to `destroy_by`.
-    MD
   },
 
   {
@@ -61,12 +53,12 @@ Never pass user input directly to `destroy_by`.
     :action => :from_method,
     :name => "From Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-from",
-    :query => "User.from(payload[:from]).where(admin: false).all",
+    :query => "User.from(payload[:from]).where(credit_card: false)",
     :input => {:name => :from, :example => "users WHERE admin = '1' OR ''=?;"},
     :example => "Instead of returning all non-admin users, we return all admin users.",
     :sql => "REPLACE",
     :desc => <<-MD
-The `from` method accepts arbitrary SQL.
+The `from` method accepts arbitrary SQL. PROBLEM: think of an example where the from table is user controlled.
     MD
   },
 
@@ -74,11 +66,11 @@ The `from` method accepts arbitrary SQL.
     :action => :group_method,
     :name => "Group Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-group",
-    :query => "User.where(:admin => false).group(payload[:group])",
-    :input => {:name => :group, :example => "name UNION SELECT * FROM users"},
+    :query => "User.where(is_admin: false).group(payload[:group])",
+    :input => {:name => :group, :example => "product_id"},
     :example => "The intent of this query is to group non-admin users by the specified column. Instead, the query returns all users.",
-    :sql => "REPLACE",
-    :desc => "The `group` method accepts arbitrary SQL strings."
+    :sql => "SELECT order_products.[REPLACE], sum(quantity) FROM order_products GROUP BY order_products.[REPLACE] LIMIT $1",
+    :prob => "PROBLEM, the column referenced by group_by must exist in the select clause per SQL syntax.  If both are dynamic, then the injection would be into the select clause not the group clause"
   },
 
   {
@@ -89,6 +81,7 @@ The `from` method accepts arbitrary SQL.
     :input => {:name => :total, :example => "1) UNION SELECT * FROM orders--"},
     :example => "This input injects a union in order to return all orders, instead of just the orders from a single user.",
     :sql => "REPLACE",
+    :prob => "PROBLEM, the column referenced by group_by must exist in the select clause per SQL syntax.  If both are dynamic, then the injection would be into the select clause not the group clause",
     :desc => "The `having` method does not escape its input and is easy to use for SQL injection since it tends to be at the end of a query."
   },
 
@@ -101,7 +94,8 @@ The `from` method accepts arbitrary SQL.
     :input => {:name => :table, :example => "--"},
     :example => 'Skip WHERE clause and return all orders instead of just the orders for the specified user.',
     :sql => "REPLACE",
-    :desc => 'The `joins` method can take an array of associations or straight SQL strings.'
+    :desc => 'The `joins` method can take an array of associations or straight SQL strings.',
+    :prob => 'Again, when would the table name for a join be user controlled input'
   },
 
   {
@@ -110,24 +104,18 @@ The `from` method accepts arbitrary SQL.
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-lock",
     :query => "User.where('id > 1').lock(payload[:lock])",
     :input => {:name => :lock, :example => "?"},
-    :example => "Not a real example: SQLite does not support this option.",
-    :sql => "REPLACE",
-    :desc => <<-MD
-The `lock` method and the `:lock` option for `find` and related methods accepts a SQL fragment.
-    MD
+    :example => "The value is database dependent.  Unlikely to be a user controlled variable, but may be a hidden parameter designed to control behavior of different queries executed from the same form. Postgres values include FOR UPDATE, FOR SHARE, and FOR NO KEY UPDATE.  The value specified is directly appended to the end of the query, but after the LIMIT clause so it is too late for a UNION.  Can add OFFSET to select a different record than intended by the application e.g. OFFSET 1",
+    :sql => "SELECT users.* FROM users WHERE (id > 1) LIMIT $1 [REPLACE]",
   },
 
   {
     :action => :not,
     :name => "Not Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods/WhereChain.html#method-i-not",
-    :query => 'User.where.not("admin = 1 OR id IN (#{payload[:excluded]})").all',
-    :input => {:name => :excluded, :example => "?)) OR 1=1 --"},
+    :query => 'User.where.not("is_admin = true OR id IN (#{payload[:excluded]})").all',
+    :input => {:name => :excluded, :example => "1)) OR 1=1 --"},
     :example => "Return all users, even if they are administrators.",
-    :sql => "REPLACE",
-    :desc => <<-MD
-The `not` method is equivalent to `where` and is equally unsafe when passed SQL strings directly.
-    MD
+    :sql => "SELECT users.* FROM users WHERE NOT (is_admin = true OR id IN ([REPLACE])) LIMIT $1",
   },
 
   {
@@ -135,10 +123,9 @@ The `not` method is equivalent to `where` and is equally unsafe when passed SQL 
     :name => 'Select Method',
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-select",
     :query => 'User.select(payload[:column])',
-    :input => {:name => :column, :example => "* FROM users WHERE admin = '1' ;"},
+    :input => {:name => :column, :example => "* FROM USERS WHERE 0!=$1; --"},
     :example => 'Since the `SELECT` clause is at the beginning of the query, nearly any SQL can be injected.',
-    :sql => "REPLACE",
-   :desc => 'The `:select` method allows complete control over the `SELECT` clause of the query.'
+    :sql => "SELECT [REPLACE] FROM users LIMIT $1",
   },
 
   {
@@ -146,50 +133,39 @@ The `not` method is equivalent to `where` and is equally unsafe when passed SQL 
     :name => 'Reselect Method',
     :link => 'https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-reselect',
     :query => 'User.select(:name).reselect(payload[:column])',
-    :input => {:name => :column, :example => "* FROM orders -- "},
+    :input => {:name => :column, :example => "* FROM orders WHERE 0!=$1;-- "},
     :example => 'This is the same as `select`. Since the `SELECT` clause is at the beginning of the query, nearly any SQL can be injected, including querying totally different tables than intended.',
-    :sql => "REPLACE",
-    :desc => 'The `reselect` method allows complete control over the `SELECT` clause of the query.'
+    :sql => "SELECT [REPLACE] FROM users LIMIT $1",
   },
 
   {
     :action => :where,
     :name => "Where Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-where",
-    :query => 'User.where("first_name = \'#{payload[:first_name]}\' AND password = \'#{params[:password]}\'")',
+    :query => 'User.where("first_name = \'#{payload[:first_name]}\' AND pw_hash = \'SUBMITTED_PASSWORD\'")',
     :input => {:name => :first_name, :example => "') OR 1--"},
     :example => 'The example below is using classic SQL injection to bypass authentication.',
-    :sql => "REPLACE",
-    :desc => <<-MD
-The `where` method can be passed a straight SQL string. Calls using a hash of name-value pairs are escaped, and the array form can be used for safely parameterizing queries.
-     MD
+    :sql => "SELECT users.* FROM users WHERE (first_name = '[REPLACE]' AND pw_hash = 'SUBMITTED_PASSWORD') LIMIT $1",
   },
 
   {
     :action => :rewhere,
     :name => "Rewhere Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/QueryMethods.html#method-i-rewhere",
-    :query => 'User.where(name: "Bob").rewhere("age > #{payload[:age]}")',
-    :input => {:name => :age, :example => '1=1) OR 1=1--'},
-    :example => 'Find all users, regardless of name or age.',
-    :sql => "REPLACE",
-    :desc => <<-MD
-  Like `where`, the `rewhere` method can be passed a straight SQL string. `rewhere` adds the new conditions as a conjunction using `AND`.
-  Calls using a hash of name-value pairs are escaped, and the array form can be used for safely parameterizing queries.
-   MD
+    :query => 'User.where(first_name: "Bob").rewhere("email LIKE \'#{payload[:search]}%\'")',
+    :input => {:name => :search, :example => "a') OR 0!=$2--'"},
+    :example => 'Adds a new clause to the statement connected using AND',
+    :sql => "SELECT users.* FROM users WHERE users.first_name = $1 AND (email LIKE '[REPLACE]%') LIMIT $2",
   },
 
   {
     :action => :update_all_method,
     :name => "Update All Method",
     :link => "https://api.rubyonrails.org/v6.1.4/classes/ActiveRecord/Relation.html#method-i-update_all",
-    :query => 'User.update_all("admin = 1 WHERE name LIKE \'%#{payload[:name]}%\'")',
-    :input => {:name => :name, :example => '\' OR 1=1;'},
+    :query => 'User.update_all("is_admin = true WHERE id IN (#{payload[:id_list]})")',
+    :input => {:name => :id_list, :example => '\' OR 1=1;'},
     :example => "Update every user to be an admin.",
-    :sql => "REPLACE",
-    :desc => <<-MD
-`update_all` accepts any SQL as a string.
-User input should never be passed directly to `update_all`, only as values in a hash table.
-    MD
+    :sql => "UPDATE users SET is_admin = true WHERE id IN ([REPLACE])",
+    :prob => "Generates correct SQL, but doesn't modify the databse"
   },
 ]
