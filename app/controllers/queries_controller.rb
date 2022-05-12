dc2036c5c65580780ba9fe34e832dea5efb22ba0
Heidi Hoopes <heidih@gmail.com>
2022-05-11 23:04:14 -0600

    Improve extra information experience

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,19 +1,19 @@
-class SearchesController < ApplicationController
+class QueriesController < ApplicationController
   def index 
     payload = query_params
     if payload[:query_action].present?
       begin
-      query_type =  Queries.detect {|q| q[:action] == payload[:query_action].to_sym }
+      query_type = Queries.detect {|q| q[:action] == payload[:query_action].to_sym }
       payload[query_type[:input][:name]] = payload[:sql_string]
       
       @results = eval(query_type[:query])
-      rescue StandardError, PG::SyntaxError, PG::UndefinedColumn => e
+      rescue StandardError => e
             @error = e
       end
     end
     if query_type
-    @sql_string = query_type[:sql].gsub("[REPLACE]", payload[:sql_string])
-    @previous_params = payload
+      @sql_string = query_type[:sql].gsub("'REPLACE'", payload[:sql_string])
+      @previous_params = payload
     end
     @queries = Queries
   end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,27 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery3
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery.cookie
+//= require jquery.ui.all
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require popper
 //= require custom/buttons
+
+export function jqResetForm(form) {
+  document.getElementById('textarea')
+    .value = '';
+  document.getElementById('query_action').options.length = 0;
+}
+
+$('#query_action').change(function () {
+  var idToShow = $('#query_action').find(":selected").val();
+
+  $(".selected_content").each(function () {
+    $(this).css("display", $(this).is("#" + idToShow) ? 'block' : 'none');
+  })
+})
+
+window.jqResetForm = jqResetForm;
