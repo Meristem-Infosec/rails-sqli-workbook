9fbb92cb8555fde5128b4a97bbb31cc8dff7d8eb
Heidi Hoopes <heidih@gmail.com>
2022-05-12 21:12:52 -0600

    Add nav, better db handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :queries, only: [:index]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy, :reset]
   root to: "sessions#new"
+  get "/reset", to: "sessions#reset"
 end
